59b1b3b78f0db7dea5038d808693c4a41f3f613c
wladyslaw-kuczerenko <wladyslaw.kuczerenko@wundermanthompson.com>
2023-02-10 20:08:11 +0100

    Move link to fonts in a correct place

--- app.vue
+++ app.vue
@@ -1,4 +1,16 @@
 <template>
+  <Html lang="en">
+    <Head>
+      <Title>V-Forum</Title>
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Link rel="preconnect" href="https://fonts.gstatic.com" />
+      <Link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap"
+      />
+    </Head>
+  </Html>
+
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,24 +12,6 @@ export default defineNuxtConfig({
             }
         }
     },
-    app: {
-        head: {
-            link: [
-                {
-                    rel: 'preconnect',
-                    href:'https://fonts.googleapis.com'
-                },
-                {
-                    rel: 'preconnect',
-                    href: 'https://fonts.gstatic.com'
-                },
-                {
-                    rel: 'stylesheet',
-                    href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap'
-                }
-            ]
-        }
-    },
     css: [
         "@/assets/scss/main.scss"
     ],
