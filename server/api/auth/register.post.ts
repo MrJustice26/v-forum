d81b16c54536d5d5054dee7ecbc65986d14f429d
s481809 <wlakuc@st.amu.edu.pl>
2023-03-06 23:43:59 +0100

    FORUM-1 Implement mail service

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,10 +1,13 @@
 import userService from "~/server/services/user-service";
+import mailService from "~/server/services/mail-service";
 
 export default defineEventHandler(async (event) => {
     const {email, password} = await readBody(event);
     const userData = await userService.register(email, password);
     if(!userData) return;
-    
+
+    await mailService.sendActivationLink(userData.user.id);
+
     setCookie(event, 'token', userData.refreshToken, {httpOnly: true, maxAge: 6 * 60 * 60 * 1000 })
     setHeader(event, 'Authorization', `Bearer ${userData.accessToken}`)
 
