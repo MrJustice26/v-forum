c8bfaeb8b8074b6fdb675879617bac2710177e9b
s481809 <wlakuc@st.amu.edu.pl>
2023-03-13 00:29:58 +0100

    VFRM-1 Disable middleware for a while (TODO!)

--- middleware/authChecker.global.ts
+++ middleware/authChecker.global.ts
@@ -1,11 +1,12 @@
 import { useAuthStore } from "~/stores/auth";
 export default defineNuxtRouteMiddleware((to) => {
-    const authStore = useAuthStore();
-    const authRoutes = ['/auth/login', '/auth/register']
-    authStore.$subscribe(() => {
-        if(authStore.isAuth){
-            return navigateTo("/")
-        }
-        return navigateTo(authRoutes[0]);
-    })
-})
+  // TODO Rework this!
+  // const authStore = useAuthStore();
+  // const authRoutes = ['/auth/login', '/auth/register']
+  // authStore.$subscribe(() => {
+  //     if(authStore.isAuth){
+  //         return navigateTo("/")
+  //     }
+  //     return navigateTo(authRoutes[0]);
+  // })
+});
