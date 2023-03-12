637ddcea72afb78bf352e13d60325b4b23f820a9
s481809 <wlakuc@st.amu.edu.pl>
2023-03-13 00:29:22 +0100

    VFRM-1 Add API errors

--- server/exceptions/api-error.ts
+++ server/exceptions/api-error.ts
@@ -1,20 +1,41 @@
-
 class ApiError {
-    userAlreadyExists(){
-        throw createError({statusCode: 401, statusMessage: "User is already exists"})
-    }
+  userAlreadyExists() {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "User is already exists",
+    });
+  }
 
-    unAuthorized(){
-        throw createError({statusCode: 401, statusMessage: "Unauthorized"})
-    }
+  unAuthorized() {
+    throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
+  }
 
-    loginCredentialsIncorrect(){
-        throw createError({statusCode: 401, statusMessage: "Email or password are incorrect!"})
-    }
+  loginCredentialsIncorrect() {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Email or password are incorrect!",
+    });
+  }
 
-    internalError(){
-        throw createError({statusCode: 500, statusMessage: "Something went wrong!"})
-    }
+  invalidActivationLink() {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Activation link is not valid!",
+    });
+  }
+  userEmailAreadyActivated() {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "User email is already activated!",
+    });
+  }
+
+  internalError() {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Something went wrong!",
+    });
+  }
 }
 
 export default new ApiError();
