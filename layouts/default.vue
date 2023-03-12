2805b495fe92891c175b384f401c1546d3fe9c55
s481809 <wlakuc@st.amu.edu.pl>
2023-03-13 00:28:05 +0100

    VFRM-1 Remove redundant logic for main layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,4 @@
 <template>
-  <div v-show="isFetched">
-    <TheHeader />
-    <slot />
-  </div>
-  <div v-show="!isFetched">
-    <h1>Loading ...</h1>
-  </div>
+  <TheHeader />
+  <slot />
 </template>
-
-<script setup lang="ts">
-import { useAuthStore } from "~~/stores/auth";
-
-const authStore = useAuthStore();
-const isFetched = computed(() => authStore.getFetchingStatus);
-</script>
