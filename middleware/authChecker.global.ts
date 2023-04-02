56e3f6e98ec703c0cfe9e5a96cc0e26d1ea385e5
s481809 <wlakuc@st.amu.edu.pl>
2023-04-03 01:18:28 +0200

    VFRM Add simple fixes for authChecker

--- middleware/authChecker.global.ts
+++ middleware/authChecker.global.ts
@@ -1,12 +1,16 @@
-import { useAuthStore } from "~/stores/auth";
-export default defineNuxtRouteMiddleware((to) => {
-  // TODO Rework this!
-  // const authStore = useAuthStore();
-  // const authRoutes = ['/auth/login', '/auth/register']
-  // authStore.$subscribe(() => {
-  //     if(authStore.isAuth){
-  //         return navigateTo("/")
-  //     }
-  //     return navigateTo(authRoutes[0]);
-  // })
-});
+import { useAuthStore } from '~/stores/auth'
+export default defineNuxtRouteMiddleware((to, from) => {
+    const authStore = useAuthStore()
+    const authRoutes = ['/auth/login', '/auth/register']
+    const authActivation = '/auth/activate'
+    if (to.path.startsWith(authActivation)) return
+    if (!authStore.isAuth && !authRoutes.includes(to.path)) {
+        return navigateTo(authRoutes[0])
+    }
+    authStore.$subscribe(() => {
+        if (authStore.isAuth) {
+            return navigateTo('/')
+        }
+        return navigateTo(authRoutes[0])
+    })
+})
