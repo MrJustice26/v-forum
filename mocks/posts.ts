e4a8f4823cb475d4c18a5afd2c4c96078b06f5dd
s481809 <wlakuc@st.amu.edu.pl>
2023-04-03 01:18:15 +0200

    VFRM Add mock for posts

---
+++ mocks/posts.ts
@@ -0,0 +1,24 @@
+import IPostModel from '~~/server/models/Post/post-model.type'
+export const mockPosts: IPostModel[] = [
+    {
+        author: 'Amigo',
+        title: 'What do you think about weather today?',
+        content: 'Haha lol u died',
+        createdAt: '2022-04-01T09:45:51.082Z',
+        score: -150,
+    },
+    {
+        author: 'Amigo',
+        title: 'What do you think about weather today?',
+        content: 'Haha lol u died',
+        createdAt: '2023-02-01T09:45:51.082Z',
+        score: -150,
+    },
+    {
+        author: 'Amigo',
+        title: 'How to ...?',
+        content: 'Haha lol u died',
+        createdAt: '2023-04-01T09:45:51.082Z',
+        score: -150,
+    },
+]
