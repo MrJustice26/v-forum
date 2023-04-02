a0360a1a784f036ab49647c1b8483888899ea8fa
s481809 <wlakuc@st.amu.edu.pl>
2023-04-03 01:17:48 +0200

    VFRM Add name for DB

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,18 @@
-import {Nitro} from 'nitropack';
-import mongoose from 'mongoose';
+import { Nitro } from 'nitropack'
+import mongoose from 'mongoose'
 
-
-export default async (_nitroApp: Nitro ) => {
-
-    const config = useRuntimeConfig();
+export default async (_nitroApp: Nitro) => {
+    const config = useRuntimeConfig()
 
     try {
-        mongoose.set("strictQuery", false);
-        await mongoose.connect(encodeURI(`mongodb+srv://${config.mongodbUsername}:${config.mongodbPassword}@cluster0.2ob1vjb.mongodb.net/?retryWrites=true&w=majority`));
-        console.log("Connected to MongoDB")
-    } catch(e){
-        console.error(e);
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(
+            encodeURI(
+                `mongodb+srv://${config.mongodbUsername}:${config.mongodbPassword}@cluster0.2ob1vjb.mongodb.net/forum?retryWrites=true&w=majority`
+            )
+        )
+        console.log('Connected to MongoDB')
+    } catch (e) {
+        console.error(e)
     }
-
 }
