270fcec6fb2620a10ebe5ed27153bb74a80b7efa
s481809 <wlakuc@st.amu.edu.pl>
2023-05-22 00:15:05 +0200

    EFR Disable authChecker middleware for now

--- middleware/authChecker.global.ts
+++ middleware/authChecker.global.ts
@@ -1,16 +1,17 @@
 import { useAuthStore } from '~/stores/auth'
 export default defineNuxtRouteMiddleware((to, from) => {
-    const authStore = useAuthStore()
-    const authRoutes = ['/auth/login', '/auth/register']
-    const authActivation = '/auth/activate'
-    if (to.path.startsWith(authActivation)) return
-    if (!authStore.isAuth && !authRoutes.includes(to.path)) {
-        return navigateTo(authRoutes[0])
-    }
-    authStore.$subscribe(() => {
-        if (authStore.isAuth) {
-            return navigateTo('/')
-        }
-        return navigateTo(authRoutes[0])
-    })
+    // const authStore = useAuthStore()
+    // const authRoutes = ['/auth/login', '/auth/register']
+    // const authActivation = '/auth/activate'
+    // if (to.path.startsWith(authActivation)) return
+    // console.log(authStore.isAuth)
+    // if (!authStore.isAuth && !authRoutes.includes(to.path)) {
+    //     return navigateTo(authRoutes[0])
+    // }
+    // authStore.$subscribe(() => {
+    //     if (authStore.isAuth) {
+    //         return navigateTo('/')
+    //     }
+    //     return navigateTo(authRoutes[0])
+    // })
 })
