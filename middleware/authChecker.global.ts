7bfe9eb54902a634e9f0e42cc31290ef3ef1b094
s481809 <wlakuc@st.amu.edu.pl>
2023-06-03 23:30:47 +0200

    EFR * Create FormModal component EFR * Fix authChecker middleware

--- middleware/authChecker.global.ts
+++ middleware/authChecker.global.ts
@@ -1,17 +1,16 @@
 import { useAuthStore } from '~/stores/auth'
-export default defineNuxtRouteMiddleware((to, from) => {
-    // const authStore = useAuthStore()
-    // const authRoutes = ['/auth/login', '/auth/register']
-    // const authActivation = '/auth/activate'
-    // if (to.path.startsWith(authActivation)) return
-    // console.log(authStore.isAuth)
-    // if (!authStore.isAuth && !authRoutes.includes(to.path)) {
-    //     return navigateTo(authRoutes[0])
-    // }
-    // authStore.$subscribe(() => {
-    //     if (authStore.isAuth) {
-    //         return navigateTo('/')
-    //     }
-    //     return navigateTo(authRoutes[0])
-    // })
+export default defineNuxtRouteMiddleware(async (to) => {
+    const authStore = useAuthStore()
+    const authRoutes = ['/auth/login', '/auth/register']
+    const authActivation = '/auth/activate'
+    if (to.path.startsWith(authActivation)) return
+    if (!authStore.isAuth && !authRoutes.includes(to.path)) {
+        return navigateTo(authRoutes[0])
+    }
+    authStore.$subscribe(() => {
+        if (authStore.isAuth) {
+            return navigateTo('/')
+        }
+        return navigateTo(authRoutes[0])
+    })
 })
