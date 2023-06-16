b735a17511cba609e0f4bd1c000301998086887d
s481809 <wlakuc@st.amu.edu.pl>
2023-06-16 23:44:36 +0200

    EFR * Implement BaseTooltip component EFR * Remove redundant content in main page

--- components/BaseTooltip.vue
+++ components/BaseTooltip.vue
@@ -1,7 +1,69 @@
 <template>
     <div
-        class="bg-black/50 inline-block py-2 px-4 rounded-xl text-sm font-bold relative"
+        class="relative inline-block"
+        @mouseenter="showTooltip"
+        @mouseleave="hideTooltip"
     >
-        <slot />
+        <slot></slot>
+        <KeepAlive>
+            <Transition name="fade">
+                <div
+                    v-if="state.visible"
+                    class="absolute left-1/2 -translate-x-1/2 bg-gray-700 p-2 rounded-md text-sm whitespace-nowrap"
+                    :class="defaultPropsClassNames"
+                >
+                    {{ content }}
+                </div>
+            </Transition>
+        </KeepAlive>
     </div>
 </template>
+
+<script setup lang="ts">
+interface BaseTooltip {
+    content: string
+    position?: 'top' | 'bottom'
+}
+
+const defaultProps = withDefaults(defineProps<BaseTooltip>(), {
+    position: 'top',
+})
+
+const state = reactive({
+    visible: false,
+})
+
+const showTooltip = () => {
+    state.visible = true
+}
+
+const hideTooltip = () => {
+    state.visible = false
+}
+
+const defaultPropsClassNames = computed(() => {
+    switch (defaultProps.position) {
+        case 'top':
+            return 'bottom-[105%]'
+        case 'bottom':
+            return 'top-[105%]'
+        default:
+            return ''
+    }
+})
+</script>
+
+<style scoped lang="scss">
+.fade-enter-active {
+    transition: opacity 0.15s ease-out;
+}
+
+.fade-leave-active {
+    transition: opacity 0.15s ease-in;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
