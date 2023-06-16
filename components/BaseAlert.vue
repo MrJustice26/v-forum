8da5d777eaaac8b8c18b41af8e5b4717f677fa4e
s481809 <wlakuc@st.amu.edu.pl>
2023-06-16 22:45:26 +0200

    Implement BaseAlert component

---
+++ components/BaseAlert.vue
@@ -0,0 +1,28 @@
+<template>
+    <div
+        class="py-3 px-5 font-medium border rounded-md text-white"
+        :class="alertVariantClassNames"
+    >
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+interface BaseAlert {
+    variant?: 'success' | 'error'
+}
+
+const props = defineProps<BaseAlert>()
+const alertVariantClassNames = computed(() => {
+    switch (props.variant) {
+        case 'success':
+            return 'bg-emerald-700/40 border-emerald-700'
+
+        case 'error':
+            return 'bg-red-700/40 border-red-700'
+
+        default:
+            return 'bg-gray-700/40 border-gray-700'
+    }
+})
+</script>
